9862a69af71e25e6548fcbbde8bf45aff46161bc
caueonofri <caue.onofri@gmail.com>
2023-01-14 15:04:18 -0300

    chore: adds app components

---
+++ src/app/app-cart/app-cart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-cart',
+  templateUrl: './app-cart.component.html',
+  styleUrls: ['./app-cart.component.css']
+})
+export class AppCartComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppCartComponent } from './app-cart/app-cart.component';
+import { AppProductListComponent } from './app-product-list/app-product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppCartComponent,
+    AppProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
