8d7e5c001bb015c6dfb2dace418f282703793106
caueonofri <caue.onofri@gmail.com>
2023-01-21 14:11:44 -0300

    add badge module, currency locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,6 +7,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule} from '@angular/material/input';
+import { MatBadgeModule} from '@angular/material/badge';
 import { FormsModule } from '@angular/forms';
 import { AppCartComponent } from './pages/cart/app-cart.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,10 +16,11 @@ import { AppHeaderComponent } from './shared/header/app-header.component';
 import { AppProductListComponent } from './pages/product-list/app-product-list.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-import ptBr from '@angular/common/locales/pt';
+import localePT from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
-registerLocaleData(ptBr)
+
+registerLocaleData(localePT);
 
 @NgModule({
   declarations: [
@@ -38,10 +40,14 @@ registerLocaleData(ptBr)
     MatToolbarModule,
     MatIconModule,
     MatInputModule,
+    MatBadgeModule,
     FormsModule,
     AppRoutingModule
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'pt'}],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {provide:DEFAULT_CURRENCY_CODE, useValue: 'BRL'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
