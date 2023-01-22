463c752bf3d2c82bc83493e13b1f635736f8b20c
caueonofri <caue.onofri@gmail.com>
2023-01-22 19:29:41 -0300

    chore: update cart logic using localstorage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,43 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Product, productList } from 'src/helpers/products';
 
-interface CartItem {
+export interface CartItem {
   product: Product,
   quantity: number
 }
-
-// to do:
-// - add quantity functionality to cart items
-// - add
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  cartContent:Array<CartItem> = [];
+  cartContent: Array<CartItem> = [];
   productList: Array<Product> = productList;
 
   addToCart(productId:number):void {
     const item:Product = this.productList.filter(a => a.id == productId)[0];
-    this.cartContent.push({product: item, quantity: 1});
+    let foundIndex:number = this.cartContent.findIndex(a => a.product.id == productId);
+    if(foundIndex >= 0) {
+      this.cartContent[foundIndex].quantity++;
+    } else {
+      this.cartContent.push({product: item, quantity: 1});
+    }
+    localStorage.setItem('cartContent', JSON.stringify(this.cartContent));
+    this.cartCount();
   }
 
   removeFromCart(productId:number):void {
     this.cartContent = this.cartContent.filter(a => a.product.id != productId);
+    this.cartCount();
   }
 
-  getItems():Array<CartItem> {
-    return this.cartContent;
+  cartCount():Observable<number>{
+    let count:number = 0;
+    this.cartContent.forEach(a => count = count + a.quantity);
+    localStorage.setItem('cartCount', JSON.stringify(count));
+    return of(count);
   }
 
-  updateQuantity(productId:number, increase:boolean):void {
-    const itemIndex = this.cartContent.findIndex(a => a.product.id == productId);
-    if(increase) {
-      this.cartContent[itemIndex].quantity++
-    } else {
-      this.cartContent[itemIndex].quantity--
-    }
-    console.log(`${this.cartContent[itemIndex]}`);
+  getItems():Observable<Array<CartItem>> {
+    return of(this.cartContent);
   }
 
 }
